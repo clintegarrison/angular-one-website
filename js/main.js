8a14a95ad8de646cef0060ef41658bceb4b08d13
cxg5940 <clint_garrison@homedepot.com>
2017-05-29 23:51:18 -0400

    added ng repeat in side bar nav with custom icons

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
+'use strict'
+
 var app = angular.module('clintegarrison', []);
+
+app.controller('SideNavController', ['$scope',
+  function($scope){
+    $scope.myName = "Clint Garrison";
+    $scope.mySkills = [
+      {
+        "Skill": "Database",
+        "icon": "./images/database.png"
+      },
+      {
+        "Skill": "Server Side",
+        "icon": "./images/server.png"
+      },
+      {
+        "Skill": "Mobile",
+        "icon": "./images/mobile.png"
+      },
+      {
+        "Skill": "User Interface",
+        "icon": "./images/desktop.png"
+      },
+    ]
+  }]);
